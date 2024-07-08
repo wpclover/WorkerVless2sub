db5e5cb78b65f264bc4567afa13acb5b297cb382
wpclover <45108440+wpclover@users.noreply.github.com>
2024-07-09 03:04:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -237,7 +237,7 @@ async function nginx() {
 	<!DOCTYPE html>
 	<html>
 	<head>
-	<title>Welcome to nginx!</title>
+	<title>Welcome WPC</title>
 	<style>
 		body {
 			width: 35em;
@@ -247,16 +247,8 @@ async function nginx() {
 	</style>
 	</head>
 	<body>
-	<h1>Welcome to nginx!</h1>
-	<p>If you see this page, the nginx web server is successfully installed and
-	working. Further configuration is required.</p>
-	
-	<p>For online documentation and support please refer to
-	<a href="http://nginx.org/">nginx.org</a>.<br/>
-	Commercial support is available at
-	<a href="http://nginx.com/">nginx.com</a>.</p>
-	
-	<p><em>Thank you for using nginx.</em></p>
+	<h1>Hello!</h1>
+	<p>WPC</p>
 	</body>
 	</html>
 	`
